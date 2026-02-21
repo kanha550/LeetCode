77a253e5a1dae3a486069d9a9fad777fbd7ede79
kanhaiya mishra <kanhaiyamishra2005@gmail.com>
2026-02-21 12:34:53 +0530

    cors

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -69,8 +69,13 @@ const videoRouter = require("./routes/videoCreator");
 
 const cors = require('cors');
 
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+//   credentials: true
+// }));
+
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ["http://localhost:5173", "https://13.234.17.43"],
   credentials: true
 }));
 
